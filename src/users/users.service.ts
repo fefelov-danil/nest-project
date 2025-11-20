21c3918d1166e74474760f6400d52c5af0fdf883
Danil Fefelov <fefelov.danil@gmail.com>
2025-11-20 07:53:12 +0300

    [added] findAll in users.controller.ts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    { id: 1, name: 'Danil', age: 34 },
+    { id: 2, name: 'Anastasiya', age: 32 },
+  ];
+
+  findAll() {
+    return this.users;
+  }
+}
