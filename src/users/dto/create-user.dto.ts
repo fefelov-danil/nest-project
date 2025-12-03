d421ff43eb7e81a4b19af374b6c084558337e7fe
Danil Fefelov <fefelov.danil@gmail.com>
2025-12-03 08:01:52 +0300

    [added] decorators for CreateUserDto validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,10 @@
+import { IsNotEmpty, IsNumber, IsString, Length } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @Length(2, 20)
   name: string;
+  @IsNumber()
   age: number;
 }
